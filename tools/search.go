15ed1f80d60ca2c61f8e5988fe0b00f5ce9f97cf
Yasir Ekinci <ekinci.yasir@gmail.com>
2025-04-30 10:01:27 +0200

    improve dashboard search
    - Improve instructions based on search behaviour (substring exact matching)
    - Query can be optional when searching for all dashboards (default limit 1000)
    - Support extra filtering on tag, uid and folderUID

--- tools/search.go
+++ tools/search.go
@@ -14,26 +14,40 @@ import (
 var dashboardTypeStr = "dash-db"
 
 type SearchDashboardsParams struct {
-	Query string `json:"query" jsonschema:"description=The query to search for"`
+	Query     string   `json:"query,omitempty" jsonschema:"description=Single keyword or phrase for case-insensitive substring matching within the dashboard title. The search looks for the *exact* substring provided. Do NOT combine multiple terms."`
+	Tag       []string `json:"tag,omitempty" jsonschema:"description=List of dashboard tags to search for. Must be an array\\, even if searching for a single tag. Example: [\"production\"]"`
+	UID       []string `json:"dashboardUID,omitempty" jsonschema:"description=List of dashboard UIDs to search for. Must be an array\\, even if searching for a single UID. Example: [\"abc-123\"]"`
+	FolderUID []string `json:"folderUID,omitempty" jsonschema:"description=List of folder UIDs to search in. Must be an array\\, even if searching for a single UID. Example: [\"folder-xyz\"]"`
 }
 
 func searchDashboards(ctx context.Context, args SearchDashboardsParams) (models.HitList, error) {
 	c := mcpgrafana.GrafanaClientFromContext(ctx)
 	params := search.NewSearchParamsWithContext(ctx)
+	params.SetType(&dashboardTypeStr)
+
 	if args.Query != "" {
 		params.SetQuery(&args.Query)
-		params.SetType(&dashboardTypeStr)
 	}
-	search, err := c.Search.Search(params)
+	if len(args.Tag) > 0 {
+		params.SetTag(args.Tag)
+	}
+	if len(args.UID) > 0 {
+		params.SetDashboardUIDs(args.UID)
+	}
+	if len(args.FolderUID) > 0 {
+		params.SetFolderUIDs(args.FolderUID)
+	}
+
+	searchResult, err := c.Search.Search(params)
 	if err != nil {
-		return nil, fmt.Errorf("search dashboards for %+v: %w", c, err)
+		return nil, fmt.Errorf("failed to search dashboards with params %+v: %w", args, err)
 	}
-	return search.Payload, nil
+	return searchResult.Payload, nil
 }
 
 var SearchDashboards = mcpgrafana.MustTool(
 	"search_dashboards",
-	"Search for Grafana dashboards by a query string. Returns a list of matching dashboards with details like title, UID, folder, tags, and URL.",
+	"Search for Grafana dashboards by title query, tags, UIDs, or folder UID. Returns a list of matching dashboards with details like title, UID, folder, tags, and URL.",
 	searchDashboards,
 )
 
